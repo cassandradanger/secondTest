52b3fc178ca3826f9330ffb167183e56de9eac19
Casie Lynch <cds.lynch@gmail.com>
2015-08-20 13:57:26 -0500

    update port

--- server/app.js
+++ server/app.js
@@ -4,12 +4,13 @@ var bodyParser = require('body-parser');
 
 var index = require("./routes/index");
 
-var server = app.listen(3000, function(){
-    var port = server.address().port;
-    console.log("Listening on port: ", port);
-});
-
 app.use(bodyParser.json());
 app.use("/", index);
+
+app.set("port", (process.env.PORT || 5000));
+
+app.listen(app.get("port"), function(){
+    console.log("Listening on port: " + app.get("port"));
+});
 
 module.exports = app;
